27a8057a063c2227fcf89a8200d0475d2024e9b2
AhmedElTabarani <eltabaraniahmed@gmail.com>
2022-04-18 17:32:15 +0200

    تقسيم الأكواد إلى دوال

--- dorarAPI.js
+++ dorarAPI.js
@@ -1,27 +1,45 @@
 const getJSON = require('get-json');
 const { decode } = require('html-entities');
 module.exports = async (query) => {
-  const json = [];
   query.page = query.page || 1;
   const url = `https://dorar.net/dorar_api.json?skey=${query.value}&page=${query.page}`;
 
   const data = await getJSON(encodeURI(url));
   const html = decode(data.ahadith.result);
-  const allHadith = html.matchAll(/<div class="hadith".*?>(.*?)<\/div>/g);
-  const allHadithInfo = html.matchAll(
-    /<div class="hadith-info">([\s\S]*?)<\/div>/g
-  );
-  for (const hadith of allHadith) {
+  const allHadith = getAllHadith(html);
+  const allHadithInfo = getAllHadithInfo(html);
+
+  const result = allHadith.map((hadith, index) => {
+    return {
+      ...hadith,
+      ...allHadithInfo[index],
+    };
+  });
+  return result;
+};
+
+const getAllHadith = (html) => {
+  const allHadith = [];
+  const allHadithHTML = html.matchAll(/<div class="hadith".*?>(.*?)<\/div>/g);
+  for (const hadith of allHadithHTML) {
     const _hadith = hadith[1]
       .replace(/<\/?[^>]+(>|$)/g, '')
       .replace(/^\d+ -/g, '')
       .trim();
-    json.push({
+
+    allHadith.push({
       hadith: _hadith,
     });
   }
-  let i = 0;
-  for (const hadithInfo of allHadithInfo) {
+  return allHadith;
+};
+
+const getAllHadithInfo = (html) => {
+  const allHadithInfo = [];
+  const allHadithInfoHTML = html.matchAll(
+    /<div class="hadith-info">([\s\S]*?)<\/div>/g
+  );
+  for (const hadithInfo of allHadithInfoHTML) {
     const _hadithInfo = hadithInfo[1].replace(/<\/?[^>]+(>|$)/g, '').trim();
     const el_rawi = _hadithInfo.match(/الراوي: ([\s\S]*?) (?=المحدث)/);
     const el_mohdith = _hadithInfo.match(/المحدث: ([\s\S]*?) (?=المصدر)/);
@@ -30,12 +48,14 @@ module.exports = async (query) => {
       /الصفحة أو الرقم: ([\s\S]*?) (?=خلاصة حكم المحدث)/
     );
     const grade = _hadithInfo.match(/خلاصة حكم المحدث: ([\s\S]*?)$/);
-    json[i]['el_rawi'] = el_rawi[1].trim();
-    json[i]['el_mohdith'] = el_mohdith[1].trim();
-    json[i]['source'] = source[1].trim();
-    json[i]['number_or_page'] = number_or_page[1].trim();
-    json[i]['grade'] = grade[1].trim();
-    i++;
+
+    allHadithInfo.push({
+      el_rawi: el_rawi[1].trim(),
+      el_mohdith: el_mohdith[1].trim(),
+      source: source[1].trim(),
+      number_or_page: number_or_page[1].trim(),
+      grade: grade[1].trim(),
+    });
   }
-  return json;
+  return allHadithInfo;
 };
