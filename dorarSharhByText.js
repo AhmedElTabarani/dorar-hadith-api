9b7d2ad5037d681452cf9fee909b24eb40e624d9
AhmedEl-Tabarani <eltabaraniahmed@gmail.com>
2023-04-23 18:50:14 +0200

    fix cache

--- dorarSharhByText.js
+++ dorarSharhByText.js
@@ -15,21 +15,21 @@ module.exports = async (text, query, req, next) => {
     const res = await nodeFetch(url);
     const html = decode(await res.text());
     const doc = new JSDOM(html).window.document;
-    const result = Array.from(
-      doc.querySelectorAll(`#${req.tab} .border-bottom`)
-    )
-      .map((info) => {
-        const sharhId = info
-          .querySelector('a[xplain]')
-          ?.getAttribute('xplain');
-        return sharhId;
-      })
-      .filter((sharhId) => sharhId !== undefined)
-      .map((sharhId) => sharhById(sharhId));
+    const result = await Promise.all(
+      Array.from(doc.querySelectorAll(`#${req.tab} .border-bottom`))
+        .map((info) => {
+          const sharhId = info
+            .querySelector('a[xplain]')
+            ?.getAttribute('xplain');
+          return sharhId;
+        })
+        .filter((sharhId) => sharhId !== undefined)
+        .map((sharhId) => sharhById(sharhId))
+    );
 
     cache.set(url, result);
 
-    return await Promise.all(result);
+    return result;
   } catch (err) {
     next(new Error(err));
   }
